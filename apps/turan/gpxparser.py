fc1733e5f6eff76c4b58c5a870dd1ed0a79db638
Tor Hveem <xt@bash.no>
2009-08-19 21:47:17 +0200

    also add entries-list with posdicts

--- apps/turan/gpxparser.py
+++ apps/turan/gpxparser.py
@@ -1,16 +1,20 @@
 from xml.etree import ElementTree as ET
+import datetime
 
 class GPXParser(object):
+    start_lon = 0.0
+    start_lat = 0.0
+
+    end_lon = 0.0
+    end_lat = 0.0
+
+    entries = []
+
     def __init__(self, filename):
         doc = ET.parse(filename)
         root = doc.getroot()
         ns = root.tag[:-3]
 
-        self.start_lon = 0.0
-        self.start_lat = 0.0
-
-        self.end_lon = 0.0
-        self.end_lat = 0.0
 
         for trk in root.findall(ns + 'trk'):
             trksegs = trk.find(ns + 'trkseg')
@@ -18,11 +22,22 @@ class GPXParser(object):
             for trkpt in trkpts:
                 lat = float(trkpt.attrib['lat'])
                 lon = float(trkpt.attrib['lon'])
+
+                tstring = trkpt.getchildren()[0].text
+                time = datetime.datetime(*map(int, tstring.replace("T","-").replace(":","-").strip("Z").split("-")))
+
                 if not self.start_lon:
                     self.start_lon = lon
                     self.start_lat = lat
-                self.end_lon = lon
-                self.end_lat = lat
+
+                self.entries.append( {
+                    'time':time,
+                    'lon': lon ,
+                    'lat': lat,
+                    })
+
+            self.end_lon = lon
+            self.end_lat = lat
 
 
 if __name__ == '__main__':
@@ -30,5 +45,8 @@ if __name__ == '__main__':
     filename = sys.argv[1]
     g = GPXParser(filename)
     print g.start_lon, g.start_lat, g.end_lon, g.end_lat
+
+    for e in g.entries:
+        print e['time'], e['lon'], e['lat']
 
 
