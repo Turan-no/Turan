396c05c079f20e9f4904324cbaad9b592986fa9a
Tor Hveem <xt@bash.no>
2009-08-25 19:07:53 +0200

    eh. what?

---
+++ apps/turan/templates/turan/js_datasets.js
@@ -0,0 +1,41 @@
+{% load i18n %}
+{
+        {% if speed%}
+        "speed": {
+            data: [{{ speed}}],
+            label: "{% trans "Speed" %}",
+            color: 0,
+        },
+        {% endif %}
+        {% if hr%}
+        "hr": {
+            data: [{{ hr}} ],
+            label: "{% trans "HR" %}",
+            yaxis: 2,
+            color: 2,
+        },
+        {% endif %}
+        {% if cadence%}
+        "cadence": {
+            data: [ {{ cadence}}],
+            label: "{% trans "Cadence" %}",
+            color: 1,
+            },
+        {% endif %}
+        {% if altitude%}
+        "altitude":{
+            data: [ {{ altitude}}],
+            label: "{% trans "Altitude" %}",
+            lines: { show: true, fill: 0.3 },
+            color: 3,
+        },
+        {% endif %}
+        {% if power%}
+        "power": {
+            data: [ {{ power}}],
+            label: "{% trans "Power" %}",
+            points: { show: false } ,
+            color: 5,
+            },
+        {% endif %}
+        };
