4346d836cfc0c6b48f97d52004c740f5fb11f27a
Tor Hveem <xt@bash.no>
2009-11-27 11:25:49 +0100

    send email

---
+++ deploy/send_email.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+WORKON_HOME=/home/turan.lart.no/pinax-env
+PROJECT_ROOT=/home/turan.lart.no/pinax-env/turansite
+
+# activate virtual environment
+. $WORKON_HOME/pinax-env/bin/activate
+
+cd $PROJECT_ROOT
+python manage.py send_mail >> $PROJECT_ROOT/logs/cron_mail.log 2>&1
