7da68e97767f30fe305dc667060a56daf2e40f81
Tor Hveem <xt@bash.no>
2009-12-09 21:06:51 +0100

    new db

--- local_settings.py
+++ local_settings.py
@@ -1,8 +1,8 @@
 DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'lortal_pinax'             # Or path to database file if using sqlite3.
+DATABASE_NAME = 'turan'             # Or path to database file if using sqlite3.
 DATABASE_USER = 'lortal'             # Not used with sqlite3.
 DATABASE_PASSWORD = 'l0rt4l'         # Not used with sqlite3.
 DATABASE_HOST = 'localhost'             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
-ADMIN_MEDIA_PREFIX = '/site_media/'
+#ADMIN_MEDIA_PREFIX = '/site_media/'
