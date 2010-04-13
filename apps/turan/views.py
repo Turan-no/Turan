620a3ac2fe9964e38d7678cf686eb45a004d0aa1
Lars Eilertsen <larseie@pvv.ntnu.no>
2010-04-13 20:40:03 +0200

    Use measured power for power per weight

--- apps/turan/views.py
+++ apps/turan/views.py
@@ -875,8 +875,11 @@ def exercise(request, object_id):
             slope.speed = slope.length/slope.duration.seconds * 3.6
             slope.avg_hr = getavghr(details, slope.start, slope.end)
             slope.avg_power = calcpower(userweight, 10, slope.gradient, slope.speed/3.6)
-            slope.avg_power_kg = slope.avg_power / userweight
             slope.actual_power = getavgpwr(details, slope.start, slope.end)
+            if slope.actual_power:
+                slope.avg_power_kg = slope.actual_power / userweight
+            else:
+                slope.avg_power_kg = slope.avg_power / userweight
 
         zones = getzones(details)
         inclinesummary = getinclinesummary(details)
