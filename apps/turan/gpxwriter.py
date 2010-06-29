871e08c8c1dfe260a67c082cf43f287bdd8e1955
Tor Hveem <xt@bash.no>
2010-06-29 10:24:29 +0200

    ele first, it seems

--- apps/turan/gpxwriter.py
+++ apps/turan/gpxwriter.py
@@ -31,8 +31,8 @@ xsi:schemaLocation="http://www.topografix.com/GPX/1/0 http://www.topografix.com/
                 # Some devices (Garmin!) likes to save 0-points. Skip those.
                 return 
             xmlp = '<trkpt lat="%s" lon="%s">\n' %(lat, lon)
-            xmlp+= ' <time>%s</time>\n' %time.strftime('%Y-%m-%dT%H:%M:%SZ')
             xmlp+= ' <ele>%.1f</ele>\n' %alt
+            xmlp+= ' <time>%s</time>\n' %time.strftime('%Y-%m-%dT%H:%M:%SZ')
             xmlp+= '</trkpt>\n'
             return xmlp
 
