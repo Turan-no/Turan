6f0f06cb61ca04170a5fe64a65e2fd094a27f130
ixan <kriberg@tihlde.org>
2010-07-29 10:56:04 +0200

    Time bug fix, dist and ascent/descent calculation
    Fixes a bug when retrieving the time from a track point
    Calculates distance, ascent and descent

--- apps/turan/gpxparser.py
+++ apps/turan/gpxparser.py
@@ -1,5 +1,7 @@
 from xml.etree import ElementTree as ET
 import datetime
+import pyproj
+from math import hypot
 
 class GPXParser(object):
     start_lon = 0.0
@@ -9,12 +11,26 @@ class GPXParser(object):
     end_lat = 0.0
 
     entries = []
+    distance = 0.0
+    ascent = 0.0
+    descent = 0.0
+    geod = pyproj.Geod(ellps='WGS84')
+
+    def __proj_distance(self, lat1, lon1, lat2, lon2, elev1=None, elev2=None):
+        try:
+            hdelta = self.geod.inv(lon2, lat2, lon1, lat1)[2]
+            if elev1 and elev2:
+                distdelta = hypot(hdelta, elev1-elev2)
+            else:
+                distdelta = hdelta
+            return distdelta
+        except ValueError:
+            return 0.0
 
     def __init__(self, filename):
         doc = ET.parse(filename)
         root = doc.getroot()
         ns = root.tag[:-3]
-
 
         for trk in root.findall(ns + 'trk'):
             trksegs = trk.find(ns + 'trkseg')
@@ -22,8 +38,27 @@ class GPXParser(object):
             for trkpt in trkpts:
                 lat = float(trkpt.attrib['lat'])
                 lon = float(trkpt.attrib['lon'])
+                try:
+                    ele = float(trkpt.find(ns + 'ele').text)
+                except:
+                    ele = None
 
-                tstring = trkpt.getchildren()[0].text
+                if(len(self.entries) > 0):
+                    self.distance = self.distance + self.__proj_distance(self.entries[-1]['lat'],
+                            self.entries[-1]['lon'],
+                            lat,
+                            lon,
+                            self.entries[-1]['ele'],
+                            ele)
+                    if ele and self.entries[-1]['ele']:
+                        delta_ele = ele - self.entries[-1]['ele']
+                        if delta_ele > 0.0:
+                            self.ascent = self.ascent + delta_ele
+                        else:
+                            self.descent = self.descent + delta_ele
+
+
+                tstring = trkpt.find(ns + 'time').text
                 time = datetime.datetime(*map(int, tstring.replace("T","-").replace(":","-").strip("Z").split("-")))
 
                 if not self.start_lon:
@@ -34,6 +69,7 @@ class GPXParser(object):
                     'time':time,
                     'lon': lon ,
                     'lat': lat,
+                    'ele': ele,
                     })
 
             self.end_lon = lon
@@ -48,5 +84,8 @@ if __name__ == '__main__':
 
     for e in g.entries:
         print e['time'], e['lon'], e['lat']
+    print 'distance: ', g.distance
+    print 'ascent: ', g.ascent
+    print 'descent: ', g.descent
 
 
