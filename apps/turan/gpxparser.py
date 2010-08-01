8d85b263490944f88f475c6f21361bd328d5face
Tor Hveem <xt@bash.no>
2010-08-01 22:49:40 +0200

    use objects for entries, not dicts

--- apps/turan/gpxparser.py
+++ apps/turan/gpxparser.py
@@ -15,6 +15,17 @@ def proj_distance(lat1, lon1, lat2, lon2, elev1=None, elev2=None):
     except ValueError:
         return 0.0
 
+class GPXEntry(object):
+
+    def __init__(self, time, hr, speed, cadence, altitude, lon, lat):
+        self.time = time
+        self.hr = hr
+        self.speed = speed
+        self.cadence = cadence
+        self.altitude = altitude
+        self.lat = lon
+        self.lon = lat
+
 class GPXParser(object):
     start_lon = 0.0
     start_lat = 0.0
@@ -71,49 +82,43 @@ class GPXParser(object):
                     if vdop > vdop_cutoff:
                         # If accurarcy is low, use previous sample
                         if self.entries:
-                            ele = self.entries[-1]['altitude']
+                            ele = self.entries[-1].altitude
                     if hdop > hdop_cutoff:
                         # If accurarcy is low, use previous sample
                         if self.entries:
-                            lat = self.entries[-1]['lat']
-                            lon = self.entries[-1]['lon']
+                            lat = self.entries[-1].lat
+                            lon = self.entries[-1].lon
 
                     speed = 0
                     tstring = trkpt.find(ns + 'time').text
                     time = datetime.datetime(*map(int, tstring.replace("T","-").replace(":","-").strip("Z").split("-")))
 
                     if self.entries:
-                        this_distance = proj_distance(self.entries[-1]['lat'],
-                                self.entries[-1]['lon'],
+                        this_distance = proj_distance(self.entries[-1].lat,
+                                self.entries[-1].lon,
                                 lat,
                                 lon,
-                                self.entries[-1]['altitude'],
+                                self.entries[-1].altitude,
                                 ele)
                         self.distance += this_distance
-                        if ele and self.entries[-1]['altitude']:
-                            delta_ele = ele - self.entries[-1]['altitude']
+                        if ele and self.entries[-1].altitude:
+                            delta_ele = ele - self.entries[-1].altitude
                             if delta_ele > 0.0:
                                 self.ascent = self.ascent + delta_ele
                             else:
                                 self.descent = self.descent + delta_ele
-                        speed = 3.6 * this_distance/(time - self.entries[-1]['time']).seconds
+                        speed = 3.6 * this_distance/(time - self.entries[-1].time).seconds
 
-                    e_dict = {
-                        'time':time,
-                        'lon': lon ,
-                        'lat': lat,
-                        'altitude': ele,
-                        }
-                    e_dict['speed'] = speed
+                    e = GPXEntry(time, 0, speed, 0, ele, lon, lat)
                     self.avg_speed += speed
                     self.max_speed = max(self.max_speed, speed)
 
-                    self.entries.append(e_dict)
+                    self.entries.append(e)
 
         if self.entries:
 
-            self.start_lon = self.entries[0]['lon']
-            self.start_lat = self.entries[0]['lat']
+            self.start_lon = self.entries[0].lon
+            self.start_lat = self.entries[0].lat
 
             self.end_lon = lon
             self.end_lat = lat
@@ -122,9 +127,9 @@ class GPXParser(object):
             #self.distance = self.distance/1000
             self.distance_sum = self.distance
             self.avg_speed = self.avg_speed/len(self.entries)
-            s_t = self.entries[0]['time']
+            s_t = self.entries[0].time
             self.start_time = datetime.time(s_t.hour, s_t.minute, s_t.second)
-            self.duration = '%ss' %((self.entries[-1]['time'] - s_t).seconds)
+            self.duration = '%ss' %((self.entries[-1].time - s_t).seconds)
             self.date = datetime.date(s_t.year, s_t.month, s_t.day)
 
 
@@ -135,10 +140,10 @@ if __name__ == '__main__':
     print g.start_lon, g.start_lat, g.end_lon, g.end_lat
 
     for e in g.entries:
-        if 'speed' in e and 'altitude' in e:
-            print e['time'], e['lon'], e['lat'], e['altitude'], e['speed']
-        else:
-            print e['time'], e['lon'], e['lat']
+        #if 'speed' in e and 'altitude' in e:
+        print e.time, e.lon, e.lat, e.altitude, e.speed
+        #else:
+        #    print e['time'], e['lon'], e['lat']
     print 'distance: ', g.distance
     print 'ascent: ', g.ascent
     print 'descent: ', g.descent
