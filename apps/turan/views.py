14d2d91ae55d1701a8753234f2868835a52db4ac
Tor Hveem <xt@bash.no>
2010-09-18 22:43:15 +0200

    fix broken import after celery refactor

--- apps/turan/views.py
+++ apps/turan/views.py
@@ -1,5 +1,5 @@
 from models import *
-from tasks import smoothListGaussian
+from tasks import smoothListGaussian, calcpower
 from itertools import groupby
 from forms import ExerciseForm
 from profiles.models import Profile
