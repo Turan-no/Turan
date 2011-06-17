d581c63a87e5cebb431bce53f254d09abcdc9cab
Tor Hveem <xt@bash.no>
2011-06-17 09:49:43 +0200

    update email script

--- deploy/send_email.sh
+++ deploy/send_email.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-WORKON_HOME=/home/turan.lart.no/
-PROJECT_ROOT=/home/turan.lart.no/pinax-env/turansite
+WORKON_HOME=/home/turan.no/
+PROJECT_ROOT=/home/turan.no/turansite
 
 # activate virtual environment
-. $WORKON_HOME/pinax-env/bin/activate
+. $WORKON_HOME/bin/activate
 
 cd $PROJECT_ROOT
 python manage.py send_mail >> $PROJECT_ROOT/logs/cron_mail.log 2>&1
