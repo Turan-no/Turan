1afb86cd9c58507f25dfbd009ee5c29c1bc646ea
Tor Hveem <xt@bash.no>
2011-07-21 11:03:13 +0200

    increase start offset

--- apps/turan/tasks.py
+++ apps/turan/tasks.py
@@ -266,7 +266,7 @@ def slice_to_segmentdetail(exercise, segment, start, stop):
     new_object = SegmentDetail(**data)
     new_object.save()
 
-def search_trip_for_possible_segments_matches(exercise, start_offset=30, end_offset=90, dupedistance=200, search_in_segments=None):
+def search_trip_for_possible_segments_matches(exercise, start_offset=50, end_offset=90, dupedistance=200, search_in_segments=None):
     ''' For every segment
             iterate every detail searching for pos matching the start pos
             then find match for end pos if distance elapsed doesn't exceed segment distance
