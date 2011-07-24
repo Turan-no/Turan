53a46994b7d711a0cdf323df6b3f9d705cfaedb3
Tor Hveem <xt@bash.no>
2011-07-24 21:19:27 +0200

    fix sentry group 287

--- apps/turan/views.py
+++ apps/turan/views.py
@@ -280,7 +280,7 @@ def segment_detail(request, object_id):
             usertimes[slope.exercise.user] = ''
         time = slope.duration/60
         usertimes[slope.exercise.user] += mark_safe('[%s, %s],' % (datetime2jstimestamp(slope.exercise.date), slope.duration))
-    if slope:
+    if slopes:
         exercise_type = slope.exercise.exercise_type
 
     gradients = simplejson.dumps(list(object.segmentaltitudegradient_set.values_list('xaxis', 'gradient')))
