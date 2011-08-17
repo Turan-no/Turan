266d4a49c20dbcee6ed53b9620673d08e82a4892
Tor Hveem <xt@bash.no>
2011-08-17 11:01:47 +0200

    linebreaks in wiki content

--- apps/turan/templatetags/turanwaka_tags.py
+++ apps/turan/templatetags/turanwaka_tags.py
@@ -1,6 +1,7 @@
 from django import template
 from django.utils.safestring import mark_safe
 from wakawaka.models import WikiPage, Revision
+from django.template.defaultfilters import linebreaksbr
 from django.conf import settings
 
 register = template.Library()
@@ -10,8 +11,11 @@ def wiki_help(keyword):
     p = WikiPage.objects.get(slug=keyword)
 
     if p:
+        content = p.current.content
+        # Linebreaks
+        content = linebreaksbr(content)
         # Who needs a template system
-        html = '<div class="tooltip_help" onmouseover="this.childNodes[1].style.display = \'block\'" onmouseout="this.childNodes[1].style.display = \'none\'"><img src="' + settings.MEDIA_URL + 'pinax/img/silk/icons/help.png" /><p class="tooltip_help_box" onclick="event.stopPropagation(); this.style.display = \'none\'">' + p.current.content + '</p></div>'
+        html = '<div class="tooltip_help" onmouseover="this.childNodes[1].style.display = \'block\'" onmouseout="this.childNodes[1].style.display = \'none\'"><img src="' + settings.MEDIA_URL + 'pinax/img/silk/icons/help.png" /><p class="tooltip_help_box" onclick="event.stopPropagation(); this.style.display = \'none\'">' + content + '</p></div>'
         return mark_safe(html)
     else:
         return ''
