71bb041ce4b2e7afc56469bc814abb0fea0a5ed1
Tor Hveem <xt@bash.no>
2011-09-27 09:30:59 +0200

    add API for Route

--- apps/api/handlers.py
+++ apps/api/handlers.py
@@ -1,5 +1,5 @@
 from piston.handler import BaseHandler
-from turan.models import Exercise
+from turan.models import Exercise, Route
 
 class ExerciseHandler(BaseHandler):
     allowed_methods = ('GET',)
@@ -12,6 +12,23 @@ class ExerciseHandler(BaseHandler):
 
         """
         base = Exercise.objects
+
+        if object_id:
+            return base.get(pk=object_id)
+        else:
+            return base.all()[:20]
+
+class RouteHandler(BaseHandler):
+    allowed_methods = ('GET',)
+    model = Route
+
+    def read(self, request, object_id=None):
+        """
+        Returns a single post if `blogpost_id` is given,
+        otherwise a subset.
+
+        """
+        base = Route.objects
 
         if object_id:
             return base.get(pk=object_id)
--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,14 +1,18 @@
 from django.conf.urls.defaults import *
 from piston.resource import Resource
 from api.handlers import ExerciseHandler
+from api.handlers import RouteHandler
 
 from piston.authentication import HttpBasicAuthentication, OAuthAuthentication
 from piston.doc import documentation_view
 
 exercise_handler = Resource(ExerciseHandler)
+route_handler = Resource(RouteHandler)
 auth = OAuthAuthentication(realm="Turan API")
 
 urlpatterns = patterns('',
    url(r'^exercise/(?P<object_id>\d+)/?$', exercise_handler),
    url(r'^exercise/?$', exercise_handler),
+   url(r'^route/(?P<object_id>\d+)/?$', route_handler),
+   url(r'^route/?$', route_handler),
 )
